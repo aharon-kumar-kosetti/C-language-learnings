bcfce254f617cca98cc9303a16df32ca774dd677
Aharon kosetti <221158300+aharon-kumar-kosetti@users.noreply.github.com>
2025-09-19 17:32:47 +0000

    Bulk delete

--- chapter2/practice/04problem.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-     
-    int x = 2, y=3,z=3,k=1;
-    int value = 3*x/y-z+k;
-    printf("The value of the expression 3*x/y-z+k is: %d\n", value);
-    return 0;
-}
