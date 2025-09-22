3bb6c98ceb65036af0923d5674b2e2dba4699f99
Aharon kosetti <221158300+aharon-kumar-kosetti@users.noreply.github.com>
2025-09-22 18:43:14 +0530

    Create newgst-slabs.c

---
+++ PROJECTS/newgst-slabs.c
@@ -0,0 +1,37 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int ch; float bf, af, am, sv;
+    printf("1. Medicine\n2. auto-mobiles\n3.cement\n4.groceries\n5.insurance\n>>>Select one from above to know the value of the tax:  ");
+    scanf("%d", &ch);
+    printf("Enter the amount purchased:  ");
+    scanf("%f", &am);
+    
+    if(ch==1){
+        bf = am*0.12;
+        af = am*0.05;
+        sv = bf - af;
+    }
+    else if(ch==2 || ch==3){
+        bf = am*0.28;
+        af = am*0.18;
+        sv = bf - af;
+    }
+   
+    else if(ch==4){
+        bf = am*0.18;
+        af = am*0.05;
+        sv = bf - af;
+    }
+    else if(ch==5){
+        bf = am*0.18;
+        af = am*0.12;
+        sv = bf - af;
+    }
+    printf("Before the Tax Slab Update: %.2f\n", bf + am);
+    printf("After the Tax Slab Update: %.2f\n", af + am);
+    printf("Amount You Saved: %.2f\n", sv);
+    
+    return 0;
+}
